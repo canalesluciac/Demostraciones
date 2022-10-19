5a84abd9e82e3e4801503bd6ab5fb7e5972b3238
Luli <Luli@DESKTOP-JFT9AVO>
2022-10-19 14:38:09 -0300

    Agregar archivos de proyecto.

---
+++ WindowsApp/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace WindowsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(310, 196);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(75, 23);
+            this.btnIngresar.TabIndex = 0;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(242, 102);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(47, 13);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Nombre:";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(245, 132);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(64, 13);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "Contraseña:";
+            //this.lblPassword.Click += new System.EventHandler(this.lblPassword_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(377, 102);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 3;
+
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(377, 132);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.btnIngresar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtPassword;
+    }
+}
+
---
+++ WindowsApp/Form2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsApp
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            decimal maxNota = Convert.ToDecimal(txtNota1.Text), suma = 0, promedio;
+            int cont = 0;
+            string result = "result";   
+
+            List<decimal> nota = new List<decimal>();
+            nota.Add(Convert.ToDecimal(txtNota1.Text));
+            nota.Add(Convert.ToDecimal(txtNota2.Text));
+            nota.Add(Convert.ToDecimal(txtNota3.Text));
+            nota.Add(Convert.ToDecimal(txtNota4.Text));
+
+            /* Podría guardarse cada variable de manera individual y en vez de un foreach o un for, escribir un if anidado para calcular el mayor valor,
+            pero es menos eficiente
+            decimal nota1 = Convert.ToDecimal(txtNota1.Text);
+            decimal nota2 = Convert.ToDecimal(txtNota2.Text);
+            decimal nota3 = Convert.ToDecimal(txtNota3.Text);
+            decimal nota4 = Convert.ToDecimal(txtNota4.Text);*/
+
+            foreach (decimal n in nota)
+            {
+                if(n > maxNota)
+                    maxNota = n;
+                cont++;
+                suma += n;
+            }
+            promedio = suma/cont;
+
+            // si las notas se tomaran con valor de enteros (int) podría realizarse un switch
+            if (promedio >= 9)
+                result = "Excelente";
+            else if (promedio >= 7)
+                result = "Muy bien";
+            else if (promedio >= 4)
+                result = "Regular";
+            else
+               result = "En proceso de aprendizaje";
+
+            MessageBox.Show("Mayor nota: " + maxNota + "\nPromedio: " + promedio + "\n\n" + result,"Calificacion");
+        }
+
+            
+    }
+}
